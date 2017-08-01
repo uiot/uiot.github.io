70fc615762b3a12802173c64e66fa1b9be8a08f7
Claudio Santoro <claudio.santoro@redes.unb.br>
2017-08-01 18:17:46 -0300

    More Updates

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -1,7 +1,7 @@
 //jQuery(document).foundation();
 
 function resizeControl() {
-    jQuery('#js-heightControl').css('height', jQuery(window).height() - jQuery('html').height() +'px');
+    jQuery('#js-heightControl').css('height', jQuery(window).height() - jQuery('html').height() + 'px');
 }
 
 jQuery(document).ready(function () {
@@ -10,4 +10,12 @@ jQuery(document).ready(function () {
 
 jQuery(window).resize(function () {
     resizeControl();
+});
+
+jQuery(function () {
+    jQuery(window).resize(function () {
+        jQuery('.header-container').css({'width': jQuery(window).width(), 'height': jQuery(window).height()});
+    });
+
+    jQuery(window).resize();
 });
