9864ab718c2aec4dfe0cfb4a3092b2658c005c53
Claudio Santoro <claudio.santoro@redes.unb.br>
2017-08-02 01:42:51 -0300

    New Footer, More Sections, Typed nimation

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -13,3 +13,23 @@ jQuery(document).ready(function () {
 jQuery(window).resize(function () {
     resizeControl();
 });
+
+var typed = new Typed('.typed-animated', {
+    strings: ["", "<b>Is...</b> Creating innovative <b style='color:#8385D0'>ideas.</b>",
+        "<b>Is...</b> Creating changing factor <b style='color:#8385D0'>concepts.</b>",
+        "<b>Is...</b> Connecting <b style='color:#8385D0'>things</b> together.",
+        "<b>Is...</b> Working for an <b style='color:#8385D0'>universal</b> IoT platform.",
+        "<b>Is...</b> Changing how <b style='color:#8385D0'>smart homes</b> works.",
+        "<b>Is...</b> Changing how the <b style='color:#8385D0'>IoT industry</b> leads."],
+    typeSpeed: 100,
+    onLastStringBackspaced: function (self) {
+        jQuery('.typed-animated').html("What it's the <b style='color:#8385D0'>Universal Internet of Things?</b>");
+    },
+    smartBackspace: true,
+    startDelay: 4000,
+    loop: true,
+    loopCount: 2,
+    showCursor: true,
+    backSpeed: 40,
+    cursorChar: ''
+});
