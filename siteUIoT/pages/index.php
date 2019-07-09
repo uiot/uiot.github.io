353323b0d02ccdc9f322e0ea13702afd1f76b7e6
daniel-sprado <danielsprado12@gmail.com>
2019-07-09 19:26:54 -0300

    new site

---
+++ siteUIoT/pages/index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+	<nav class="navbar navbar-expand-lg fixed-top nav_inicial" id="navbara">
+		<div class="container">
+
+			  <a class="navbar-brand" href="#inicio">
+			    <a href="#inicio"><span class="logomarca"></span></a>
+			  </a>
+			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false"
+			  aria-label="Toggle navigation">
+    		  <span class="navbar-toggler-icon"></span>
+  			  </button>
+
+			  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+				  <ul class="navbar-nav navbar-nav navbar-yea mr-auto mt-2 mt-lg-0 nav_itens">
+				    <li class="nav-item">
+				      <a class="nav-link nav_elemento" href="#about"><p>About</p></a>
+				    </li>
+				    <li class="nav-item">
+				      <a class="nav-link nav_elemento" href="#servicos"><p>Serviços</p></a>
+				    </li>
+				    <li class="nav-item">
+				      <a class="nav-link nav_elemento" href="#portfolio"><p>Portfólio</p></a>
+				    </li>
+				    <li class="nav-item">
+				      <a class="nav-link nav_elemento" href="#engnet"><p>A EngNet</p></a>
+				    </li>
+				    <li class="nav-item">
+				      <a class="nav-link nav_elemento" href="#parceiros"><p>Parceiros</p></a>
+				    </li>
+				    <li class="nav-item">
+				      <a class="nav-link nav_elemento" href="#contato"><p class="ct3">Contato</p></a>
+				    </li>
+				  </ul>
+			  </div>
+		</div>
+	</nav>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
