4df94e0a3287da38f16530c4679abc7a050545ea
dasprado12 <danielsprado12@gmail.com>
2020-10-30 22:26:46 -0300

    Changing selector style

---
+++ src/components/Team/Card.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <v-card :color="colors[index%5]" dark>
+            <div class="d-flex flex-no-wrap justify-space-between black--text">
+                
+                <div class="content-body">
+                    <v-card-title class="headline"> {{person.name}} </v-card-title>
+                    <v-card-text>
+                        {{person.area}} <br> 
+                        {{person.email}}
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn text @click="getArticles(person.key)" class="black--text mx-3"> <v-icon> mdi-arrow-down </v-icon> </v-btn>
+                        <v-spacer/>
+                        <v-icon v-if="person.linkedin" class="mx-3 black--text"> mdi-linkedin </v-icon>
+                        <v-icon v-if="person.github" class="mx-3 black--text"> mdi-github </v-icon>
+                    </v-card-actions>
+                </div>
+                <v-avatar class="ma-3" size="125" tile>
+                    <v-img :src="require(`@/assets/photos/${person.picture}.svg`)"></v-img>
+                </v-avatar>
+            </div>
+        </v-card>
+        <v-dialog v-model="dialog" width="900">
+            <list-publications :data="current_publications"/>
+        </v-dialog>
+    </div>
+</template>
+
+<script>
+import listPublications from "../commom/ListPublications";
+import { filterPublications } from "../../functions/publications.js"
+
+export default {
+    props: [ 'person', 'index' ],
+    components: { 
+        listPublications
+    },
+    data(){
+        return {
+            colors: [
+                "#91c7a0",
+                "#ffa8a8",
+                "#9199c7",
+                "#e3b1b1",
+                "#b1e3e2"
+            ],
+            dialog: false,
+            current_publications: []
+        }
+    },
+    methods: {
+        getArticles(key){
+            this.dialog = true
+            this.current_publications = this.publications(key)
+        },
+        publications(member){
+            return new filterPublications().byOneMember(member)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.content-body{
+    text-align: left;
+}
+</style>
--- src/components/Team/Team.vue
+++ src/components/Team/Team.vue
@@ -1,118 +1,49 @@
 <template>
     <div class="team">
         <v-container fluid>
-            <main-title
-                :title="`Coordenadores do projeto`"
-            />
+            <main-title :title="`Coordenadores do projeto`"/>
             <v-row>
                 <v-col v-for="(item, i) in coordination" :key="item.key" xs="12" sm="12" md="6" xl="4">
-                    <v-card :color="colors[i%5]" dark>
-                        <div class="d-flex flex-no-wrap justify-space-between black--text">
-                            <v-avatar class="ma-3" size="125" tile>
-                                <v-img src="@/assets/photos/man.svg"></v-img>
-                            </v-avatar>
-                            <div class="content-body">
-                                <v-card-title class="headline"> {{item.name}} </v-card-title>
-                                <v-card-text>
-                                    {{item.area}} <br> 
-                                    {{item.email}}
-                                </v-card-text>
-                                <v-card-actions>
-                                    <v-btn text @click="getArticles(item.key)" class="black--text mx-3"> <v-icon> mdi-arrow-down </v-icon> </v-btn>
-                                    <v-spacer/>
-                                    <v-icon class="mx-3 black--text"> mdi-linkedin </v-icon>
-                                    <v-icon class="mx-3 black--text"> mdi-github </v-icon>
-                                </v-card-actions>
-                            </div>
-                        </div>
-                    </v-card>
+                    <profile :person="item" :index="i" />
                 </v-col>
             </v-row>
-
-            <main-title
-                :title="current_option | capitalize"
-            />
-            <v-menu open-on-hover bottom offset-y>
-                <template v-slot:activator="{ on, attrs }">
-                    <v-btn text color="grey" dark v-bind="attrs" v-on="on">
-                        <v-icon> mdi-arrow-bottom-right </v-icon>
-                    </v-btn>
-                </template>
-                <v-list color="#133e79">
-                    <v-list-item v-for="(item, index) in options" :key="index" @click="filterMembers(item.name)">
-                        <v-list-item-title class="white--text">{{ item.name | capitalize }}</v-list-item-title>
-                    </v-list-item>
-                </v-list>
-            </v-menu>
-
+            <div class="options">
+                <v-row>
+                    <v-col cols="3">  </v-col>
+                    <v-col cols="6"> 
+                        <v-btn rounded outlined :class="`blue ${chardware}`" @click="filterMembers('hardware')"> Hardware </v-btn>
+                        <v-btn rounded outlined :class="`green ${csoftware}`" @click="filterMembers('software')"> Software </v-btn> 
+                        <v-btn rounded outlined :class="`red ${csecurity}`" @click="filterMembers('security')"> Security </v-btn>
+                    </v-col>
+                    <v-col cols="3">  </v-col>
+                </v-row>
+            </div>
         </v-container>
         <v-row>
             <v-col v-for="(item, i) in current_team" :key="item.key" xs="12" sm="12" md="6" xl="4">
-                <v-card :color="colors[i%5]" dark>
-                        <div class="d-flex flex-no-wrap justify-space-between black--text">
-                            <div class="content-body">
-                                <v-card-title class="headline"> {{item.name}} </v-card-title>
-                                <v-card-text>
-                                    {{item.area}} <br> 
-                                    {{item.email}}
-                                </v-card-text>
-                                <v-card-actions>
-                                    <v-btn text @click="getArticles(item.key)" class="black--text mx-3"> <v-icon> mdi-arrow-down </v-icon> </v-btn>
-                                    <v-spacer/>
-                                    <v-icon class="mx-3 black--text"> mdi-linkedin </v-icon>
-                                    <v-icon class="mx-3 black--text"> mdi-github </v-icon>
-                                </v-card-actions>
-                            </div>
-                            <v-avatar class="ma-3" size="125" tile>
-                                <v-img :src="require(`@/assets/photos/${item.picture}.svg`)"></v-img>
-                            </v-avatar>
-                        </div>
-                    </v-card>
+                <profile :person="item" :index="i" />
             </v-col>
         </v-row>
-
-        <v-dialog v-model="dialog" width="900">
-            <list-publications :data="current_publications"/>
-        </v-dialog>
     </div>    
 </template>
 
 <script>
-import listPublications from "../commom/ListPublications";
-import Members from "../../texts/Members.js";
+import profile from "./Card"
 import MainTitle from "../commom/Title";
-import { filterPublications } from "../../functions/publications.js"
 import { filterMembers } from "../../functions/members.js"
 
 export default {
     components: {
         MainTitle,
-        listPublications
+        profile
     },
     data: () => ({
-        colors: [
-            "#91c7a0",
-            "#ffa8a8",
-            "#9199c7",
-            "#e3b1b1",
-            "#b1e3e2"
-        ],
-        dialog: false,
-        members: Members,
-        selected_member: [],
-        current_option: "choose an option",
-        icons: [ 'mdi-badge-account-outline', 'mdi-linkedin', 'mdi-github',],
-        options: [ { name: "software" }, { name: "hardware" }, { name: "security" }],
+        chardware: "lighten-5",
+        csoftware: "lighten-5",
+        csecurity: "lighten-5",
         current_team: [],
-        current_publications: [],
-        headers: [
-            { text: 'Year', align: 'start', sortable: false, value: 'year',},
-            { text: '', value: '' },
-            { text: 'Title', value: 'title' },
-            { text: 'Published in', value: 'short' },
-            { text: 'Authors', value: 'authors' },
-            { text: '', value: 'data-table-expand' },
-        ],
+        current_option: "",
+        
     }),
 filters: {
     teste(value){
@@ -122,26 +53,15 @@ filters: {
     }
 },
     computed: {
-        coordination(){
-            return new filterMembers().byArea('coordination')
-        },
-        software(){
-            return new filterMembers().byArea('software')
-        },
-        hardware(){
-            return new filterMembers().byArea('hardware')
-        },
-        security(){
-            return new filterMembers().byArea('security')
-        }
+        coordination(){ return new filterMembers().byArea('coordination') },
+        software(){ return new filterMembers().byArea('software') },
+        hardware(){ return new filterMembers().byArea('hardware') },
+        security(){ return new filterMembers().byArea('security') }
     },
     methods: {
-        getArticles(key){
-            this.dialog = true
-            this.current_publications = this.publications(key)
-        },
-        filterMembers(area){        
-            this.current_option = area    
+        filterMembers(area){
+            this.current_option = area        
+            this.setSelectedColors(area)
             if(area == 'software'){
                 this.current_team = this.software
             }else if(area == 'hardware'){
@@ -150,8 +70,20 @@ filters: {
                 this.current_team = this.security
             }
         },
-        publications(member){
-            return new filterPublications().byOneMember(member)
+        setSelectedColors(area){
+            if(area == 'software'){
+                this.csoftware = "lighten-3"
+                this.chardware = "lighten-5"
+                this.csecurity = "lighten-5"
+            }else if(area == 'hardware'){
+                this.csoftware = "lighten-5"
+                this.chardware = "lighten-3"
+                this.csecurity = "lighten-5"
+            }else if(area == 'security'){
+                this.csoftware = "lighten-5"
+                this.chardware = "lighten-5"
+                this.csecurity = "lighten-3"
+            }
         }
     }
 }
@@ -162,9 +94,14 @@ filters: {
     padding: 2% 2% 10% 2%;
 }
 
-.content-body{
-    text-align: left;
+.options{
+    padding-left: 100px;
+    padding-right: 100px;
+    text-align: center;
 }
+
+
+
 
 .titulo {
     text-align: center;
