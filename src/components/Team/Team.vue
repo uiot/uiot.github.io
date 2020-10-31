c8e3ba863c869fd62ca52633a9323ce998c0f3d3
dasprado12 <danielsprado12@gmail.com>
2020-10-30 21:13:33 -0300

    bindig component listPublications to team

--- src/components/Team/Team.vue
+++ src/components/Team/Team.vue
@@ -72,43 +72,13 @@
         </v-row>
 
         <v-dialog v-model="dialog" width="900">
-            <v-card>
-                <v-data-table
-                    :headers="headers"
-                    :items="current_publications.length ? current_publications : []"
-                    class="grey lighten-5"
-                    item-key="title"
-                    :items-per-page="5"
-                    show-expand
-                >
-                    <template v-slot:body="{ items }">
-                        <tbody>
-                            <tr v-for="(item, i) in items" :key="`${i}-${item.id}`">
-                                <td> <v-chip class="base white--text"> <b> {{ item.year }} </b> </v-chip> </td>
-                                <td v-if="!item.language"></td>
-                                <td v-else-if="item.language === 'en'"> <img width="20" :src="require(`@/assets/united-states.png`)"> </td>
-                                <td v-else-if="item.language === 'pt-br'"> <img width="20" :src="require(`@/assets/brazil.png`)"> </td>
-                                <td v-else></td>
-                                <td>{{ item.title }}</td>
-                                <td><span>{{ item.short }}</span> <span v-if="!item.short" >({{ item.type }})</span> </td>
-                                <td>{{ item.authors | teste }}</td>
-                                <td>
-                                <a v-if="item.download" :href="`/assets/pdf/${item.download}`" target="_blank"> <img width="20" :src="require(`@/assets/pdf.png`)"> </a>
-                                <a v-if="item.publication_link" :href="`${item.publication_link}`" target="_blank"> <img width="20" :src="require(`@/assets/globe.png`)"> </a>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </template>
-                    <template v-slot:expanded-item="{  item }">
-                        <span v-for="author in item.authors" :key="author"> {{ author }} </span>
-                    </template>
-                </v-data-table>
-            </v-card>
+            <list-publications :data="current_publications"/>
         </v-dialog>
     </div>    
 </template>
 
 <script>
+import listPublications from "../commom/ListPublications";
 import Members from "../../texts/Members.js";
 import MainTitle from "../commom/Title";
 import { filterPublications } from "../../functions/publications.js"
@@ -116,7 +86,8 @@ import { filterMembers } from "../../functions/members.js"
 
 export default {
     components: {
-        MainTitle  
+        MainTitle,
+        listPublications
     },
     data: () => ({
         colors: [
