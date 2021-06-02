42528b8339d0b43809af18c81ee56a80e74ae6aa
VieiraHeitor <44251243+VieiraHeitor@users.noreply.github.com>
2021-06-02 12:24:54 -0300

    trigger

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ Vue.filter("capitalize", function(value) {
   if (!value) return "";
   value = value.toString();
   return value.charAt(0).toUpperCase() + value.slice(1);
+
 });
 
 Vue.config.productionTip = false;
